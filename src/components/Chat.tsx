babed59643e60297cf5d6cbbd13679ac66e5b987
denys-exp <denys.m@expletech.com>
2023-12-01 00:33:00 +0200

    change dependencies

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent, FC, useEffect, useState} from 'react'
+import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import io from "socket.io-client";
-import {useLocation, useNavigate} from "react-router-dom";
-import {messageData} from "../types";
+import { useLocation, useNavigate } from "react-router-dom";
+import { messageData } from "../types";
 import Messages from "./Messages";
 
 
@@ -14,10 +14,11 @@ const Chat: FC = () => {
   const securityOptions = {
     secure: true,
     reconnection: true,
-    rejectUnauthorized: false}
+    rejectUnauthorized: false
+  }
   const ioSocket = io('http://localhost:5000', securityOptions);
   const navigate = useNavigate();
-  const {search}= useLocation()
+  const { search } = useLocation()
   const [params, setParams] = useState<ParamsState>({});
   const [state, setState] = useState<messageData[]>([]);
   const [message, setMessage] = useState('');
@@ -33,19 +34,17 @@ const Chat: FC = () => {
   }, [search]);
 
   useEffect(() => {
-    ioSocket.on("message", ({data})=> {
+    ioSocket.on("message", ({ data }) => {
       setState(prev => ([...prev, data]))
     })
     ioSocket.on("room", ({ data: { users } }) => {
       setUsers(users.length);
     });
-  }, []);
-
-  useEffect(() => {
     ioSocket.on("messageHistory", ({ data: { messages } }) => {
       setState(prev => ([...prev, ...messages]));
     });
   }, []);
+
 
 
 
@@ -59,50 +58,50 @@ const Chat: FC = () => {
     navigate("/");
   }
 
-  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) =>  {
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault()
 
-    if(!message) return
+    if (!message) return
 
-    ioSocket.emit('sendMessage', {message, params});
+    ioSocket.emit('sendMessage', { message, params });
 
     setMessage('')
   }
 
   return (
-      <div className="flex flex-col items-center h-screen">
-        <div className="w-full bg-gray-900 flex items-center justify-between py-4 px-6 text-white">
-          <div className="text-2xl font-bold">Room: {params.room}</div>
-          <div>{users} users in this room</div>
-          <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={leftRoom}>
-            Left the room
-          </button>
-        </div>
-
-        <div className="flex-grow w-full bg-gray-800 p-4 overflow-y-auto">
-          <Messages messages={state} name={params.name} />
-
-        </div>
-
-        <form className="w-full flex justify-between bg-gray-900 p-4" onSubmit={handleSubmit}>
-          <div className="flex-grow mr-4">
-            <input
-                type="text"
-                name="message"
-                placeholder="What do you want to say?"
-                value={message}
-                onChange={handleChangeMessage}
-                autoComplete="off"
-                required
-                className="w-full h-full bg-gray-700 text-white px-4 py-2 rounded"
-            />
-          </div>
-
-          <div className="flex items-center">
-            <input type="submit" value="Send a message" className="bg-blue-600 text-white py-2 px-4 rounded cursor-pointer" />
-          </div>
-        </form>
+    <div className="flex flex-col items-center h-screen">
+      <div className="w-full bg-gray-900 flex items-center justify-between py-4 px-6 text-white">
+        <div className="text-2xl font-bold">Room: {params.room}</div>
+        <div>{users} users in this room</div>
+        <button className="bg-red-500 text-white py-2 px-4 rounded" onClick={leftRoom}>
+          Left the room
+        </button>
       </div>
+
+      <div className="flex-grow w-full bg-gray-800 p-4 overflow-y-auto">
+        <Messages messages={state} name={params.name} />
+
+      </div>
+
+      <form className="w-full flex justify-between bg-gray-900 p-4" onSubmit={handleSubmit}>
+        <div className="flex-grow mr-4">
+          <input
+            type="text"
+            name="message"
+            placeholder="What do you want to say?"
+            value={message}
+            onChange={handleChangeMessage}
+            autoComplete="off"
+            required
+            className="w-full h-full bg-gray-700 text-white px-4 py-2 rounded"
+          />
+        </div>
+
+        <div className="flex items-center">
+          <button type="submit" className="bg-blue-600 text-white py-2 px-4 rounded cursor-pointer" >Send a message</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,5 @@
-import {FC} from "react";
-import {messageData} from "../types";
+import { FC } from "react";
+import { messageData } from "../types";
 
 type MessagesProps = {
     messages?: messageData[],
@@ -7,19 +7,19 @@ type MessagesProps = {
 }
 
 const Messages: FC<MessagesProps> = ({ messages, name }) => {
-    console.log(name, 'name')
-    console.log(messages, 'name')
     return (
         <div className="flex flex-col gap-4">
             {messages?.map(({ user, message }, i) => {
                 const itsMe =
                     user.name.trim().toLowerCase() === name?.trim().toLowerCase();
 
-                const adminClass = user.name  === '' ? 'self-center p-2' : ""
+                const adminClass = user.name === '' ? 'self-center p-2' : "";
                 const messageClassName = itsMe
                     ? "self-end bg-blue-500 "
                     : "self-start bg-gray-700 ";
                 const textClassName = "p-4 rounded-xl text-white";
+
+                if (!message) return null;
 
                 return (
                     <div key={i} className={`flex flex-col min-w-[200px] ${messageClassName} ${textClassName} ${adminClass}`}>
